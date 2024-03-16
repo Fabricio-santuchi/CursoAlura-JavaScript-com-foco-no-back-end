45b8fb64213157cf83b839ca1979954cdc9eb7e4
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-03-16 18:05:52 -0300

    sv

---
+++ JavaScript(p3)__conhecendo-objetos/1-acessadno-objeto.js
@@ -0,0 +1,17 @@
+const estudante = {
+    nome: 'José silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+}
+estudante.nome // jose.
+
+function exibeInfoEstudante(objEstudante, infoEstudante){
+    return objEstudante[infoEstudante];
+}
+
+console.log(estudante['nome']);
+console.log(estudante['cpf']);
+console.log();
+console.log(exibeInfoEstudante(estudante, 'nome'));
+console.log(exibeInfoEstudante(estudante, 'cpf'));
---
+++ JavaScript(p3)__conhecendo-objetos/1-estrutura-objetos.js
@@ -0,0 +1,11 @@
+const estudante = {
+    nome: 'José silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+}
+
+console.log(estudante.nome);
+console.log(`O nome de estudante é ${estudante.nome}`);
+
+console.log(`Os três primeiros números do CPF são ${estudante.cpf.substring(0,3)}`)
