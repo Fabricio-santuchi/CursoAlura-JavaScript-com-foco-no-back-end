a57a49710ad10b897cf20acd17d61008eba64487
Fabricio santuchi <131098765+Fabricio-santuchi@users.noreply.github.com>
2024-03-17 12:09:58 -0300

    sv

---
+++ JavaScript(p3)__conhecendo-objetos/2-dados-valores.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'José silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['22999002122', '22999002123'],
+}
+
+console.log(estudante.telefones)
---
+++ JavaScript(p3)__conhecendo-objetos/2-objs-aninhados.js
@@ -0,0 +1,16 @@
+const estudante = {
+    nome: 'José silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['22999002122', '22999002123'],
+}
+
+estudante.endereco = {
+    rua: 'Rua joseph Climber',
+    numero: '45a',
+    complemento: 'apto 201',
+}
+
+console.log(estudante.endereco.rua)
